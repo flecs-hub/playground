f6b2aad4ab856d4728d6a76593dc656ab1142736
Sander Mertens <sander.mertens8@gmail.com>
2021-12-14 06:49:54 +0000

    rename db.plecs to scene.plecs

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     ECS_IMPORT(world, FlecsSystemsPhysics);
     ECS_IMPORT(world, FlecsSystemsSokol);
 
-    ecs_plecs_from_file(world, "db.plecs");
+    ecs_plecs_from_file(world, "scene.plecs");
 
     return ecs_app_run(world, &(ecs_app_desc_t) {
         .target_fps = 60, .enable_rest = true
